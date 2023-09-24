42828598c7b6b9c4976c8c19ee9dd65b57b97c13
vs76229379 <104732165+vs76229379@users.noreply.github.com>
2023-09-24 09:31:05 +0530

    update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 10000
 const mongoDB = require("./db")
 
 app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000")
+  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000","https://tomatomernappbackend.onrender.com")
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
